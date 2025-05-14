37e41efa1950b2e34329805c8ad376265348eace
George Adams <oss@geowa4.software>
2025-05-14 14:02:46 -0400

    mcp: respond with template if no matching mcp name

--- mcp-server/main.py
+++ mcp-server/main.py
@@ -10,6 +10,11 @@ from pathlib import Path
 
 from mcp.server.fastmcp import Context, FastMCP
 
+NO_MATCHING_SNIPPET_TEMPLATE = (
+    "No snippet matched '{name}'. "
+    "Call `get_snippet_summaries` to get a list of snippets matching your query."
+)
+
 
 @dataclass
 class AppContext:
@@ -25,9 +30,7 @@ async def app_lifespan(_server: FastMCP) -> AsyncIterator[AppContext]:
     snippets_path = Path(
         os.getenv("SNIPPET_DIRECTORY", Path(__file__).parent.parent / "snippets"),
     )
-    summaries = {
-        s.parent.name: s.read_text() for s in snippets_path.glob("**/SUMMARY.md")
-    }
+    summaries = {s.parent.name: s.read_text() for s in snippets_path.glob("**/SUMMARY.md")}
     yield AppContext(snippets_path=snippets_path, summaries=summaries)
 
 
@@ -35,8 +38,19 @@ mcp = FastMCP("AI Snippet Server", lifespan=app_lifespan)
 
 
 @mcp.tool()
-def get_snippet_summaries(ctx: Context) -> list[str]:
-    """Return a list of all snippet names."""
+def get_snippet_summaries(query: str, ctx: Context) -> str:  # noqa: D417
+    """Return a list of all snippet names matching the query.
+
+    You MUST call this function before `get_python_code_snippet` to obtain related snippets.
+
+    Args:
+      query: A brief query stating the goal to achieve while.
+
+    Returns:
+      Snippet names and their summaries including language, tools, and other technologies.
+
+    """
+    print(f"TODO: use query '{query}'")
     return "\n\n".join(
         [
             f"<snippet>\nSnippet Name: {name}\n\n---\n\n{summary}\n</snippet>"
@@ -46,17 +60,40 @@ def get_snippet_summaries(ctx: Context) -> list[str]:
 
 
 @mcp.tool()
-def get_python_code_snippet(name: str, ctx: Context) -> int:
-    """Get the Python snippet by name."""
-    code = ctx.request_context.lifespan_context.snippets_path / name / "main.py"
+def get_python_code_snippet(name: str, ctx: Context) -> str:  # noqa: D417
+    """Get the Python snippet by name.
+
+    You MUST call `get_snippet_summaries` first to obtain the exact snippet name.
+
+    Args:
+      name: The name of the snippet.
+
+    Returns:
+      The Python code snippet.
+
+    """
+    app_ctx: AppContext = ctx.request_context.lifespan_context
+    code = app_ctx.snippets_path / name / "main.py"
+    if not code.exists():
+        return NO_MATCHING_SNIPPET_TEMPLATE.format(name=name)
     return code.read_text()
 
 
 # Add a dynamic greeting resource
 @mcp.tool()
-def get_summary(name: str, ctx: Context) -> str:
-    """Get the summary by name."""
+def tell_me_more(name: str, ctx: Context) -> str:  # noqa: D417
+    """Tell more about the named snippet by returning its summary and README.
+
+    Optional tool to get more information about the snippet.
+
+    Args:
+      name: The name of the snippet.
+
+    Returns:
+      The summary and README of the snippet.
+
+    """
     return ctx.request_context.lifespan_context.summaries.get(
         name,
-        f"No summary matched {name}",
+        NO_MATCHING_SNIPPET_TEMPLATE.format(name=name),
     )
